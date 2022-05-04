dae06cd44206ac91807463d555e018a22a53c9f4
nathanemuyal <muyal1109@gmail.com>
2022-05-04 12:44:24 +0300

    bug

--- src/CustomRectangel.java
+++ src/CustomRectangel.java
@@ -3,42 +3,47 @@ import java.awt.*;
 public class CustomRectangel extends Canvas {
     private int x;
     private int y;
-    final int width=50;
-    final int heighet=120;
-     public String photo;
+    final int width = 50;
+    final int heighet = 120;
+    public String photo;
 
-        public int limit(int a){
-            if(a<50) {
-                return 50;
-            }else if (a>235) {
-                return 235;
-            }else {
-                return a;
-            }
+    public int limit(int a) {
+        if (a < 50) {
+            return 50;
+        } else if (a > 235) {
+            return 235;
+        } else {
+            return a;
         }
-
+    }
 
 
     public int getWidth() {
         return width;
     }
 
-    public int getX(){return this.x;}
-    public int getY(){return this.y;}
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
 
     public int getHeighet() {
         return heighet;
     }
-    public String getPhoto(){return photo;}
 
-    private Color color;
+    public String getPhoto() {
+        return photo;
+    }
 
 
     public void paint(Graphics Graphics, String pic) {
 
         Toolkit tool = Toolkit.getDefaultToolkit();
         Image i = tool.getImage(pic);
-        Graphics.drawImage(i,this.x+1,this.y,this.width,this.heighet,this);
+        Graphics.drawImage(i, this.x + 1, this.y, this.width, this.heighet, this);
     }
 
     public CustomRectangel(int x, int y, String guy) {
@@ -51,13 +56,13 @@ public class CustomRectangel extends Canvas {
 
     public void MoveRighetCar() {
         this.x = x + 5;
-        this.x=limit(x);
+        this.x = limit(x);
 
     }
 
     public void MoveLeftCar() {
         this.x = x - 5;
-        this.x=limit(x);
+        this.x = limit(x);
     }
 
     public void MoveUpCar() {
@@ -97,8 +102,9 @@ public class CustomRectangel extends Canvas {
         }
         return collision;
     }
-    public void paint(Graphics g){
-        g.setColor(this.color);
-        g.fillRect(this.x,this.y,this.width,this.heighet);
+
+    public void paint(Graphics g) {
+
+        g.fillRect(this.x, this.y, this.width, this.heighet);
     }
 }
